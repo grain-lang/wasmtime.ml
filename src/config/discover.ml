207807b319af72cf7c765d3fc43d112fcc700e38
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-11-03 14:35:45 -0700

    hack: try dune-configurator to generate flags

---
+++ src/config/discover.ml
@@ -0,0 +1,24 @@
+module C = Configurator.V1
+
+let () =
+  C.main ~name:"wasmtime" (fun c ->
+      (* let system =  |> Option.iter print_string in *)
+      (* let default_flags = ["-lpthread"] *)
+      let default : C.Pkg_config.package_conf =
+        { libs = []; cflags = [ "-O2"; "-Wall"; "-Wextra" ] }
+      in
+      let conf =
+        match C.ocaml_config_var c "system" with
+        | None -> default
+        | Some "linux" -> { default with libs = [ "-lpthread"; "-ldl"; "-lm" ] }
+        | Some "mingw" ->
+            {
+              cflags = [ "-DWASM_API_EXTERN="; "-DWASI_API_EXTERN=" ];
+              libs =
+                [ "-luserenv"; "-lole32"; "-lntdll"; "-lws2_32"; "-lkernel32" ];
+            }
+        | Some _ -> default
+      in
+
+      C.Flags.write_sexp "c_flags.sexp" conf.cflags;
+      C.Flags.write_sexp "c_library_flags.sexp" conf.libs)
